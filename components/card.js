15a1efadfd9d9120ab8982595440f6329fa9436a
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-12-26 18:27:57 +0100

    add roundedness

--- components/card.js
+++ components/card.js
@@ -1,11 +1,15 @@
+import Image from "next/image";
+
 export default function Card() {
   return (
     <>
       <div class="flex justify-center">
-        <div class="rounded-lg shadow-lg bg-white max-w-sm">
+        <div class="rounded-lg shadow-lg bg-white max-w-sm p-4">
           <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
-            <img
-              class="rounded-t-lg"
+            <Image
+            width={500}
+            height = {500}
+              className="rounded-lg"
               src="/images/image-qr-code.png"
               alt=""
             />
