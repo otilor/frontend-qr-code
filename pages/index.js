2bfcf43f3101193a62012f7fdf905057dd205d13
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-12-26 18:10:46 +0100

    add card

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import Card from '../components/card'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +16,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-
+        <Card />
       </main>
     </>
   )
