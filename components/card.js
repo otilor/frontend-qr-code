c4757edb46e2e80e87a9d052574d4d4b173df996
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-12-26 18:35:26 +0100

    remove justify

--- components/card.js
+++ components/card.js
@@ -15,13 +15,13 @@ export default function Card() {
             />
           </a>
           <div class="p-6">
-            <h5 class="text-gray-900 text-xl font-medium mb-2">Improve your front-end skills by building projects</h5>
-            <p class="text-gray-700 text-base mb-4">
+            <h5 class="text-gray-900 text-justify text-xl font-medium mb-2">Improve your front-end skills by building projects</h5>
+            <p class="text-gray-700 text-justify text-base mb-4">
               Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
             </p>
             <button
               type="button"
-              class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+              class="text-center inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
             >
               Button
             </button>
