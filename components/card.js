addde3f80c9b9992d80ecd25f9ff8881d6147805
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-12-26 18:29:53 +0100

    change text

--- components/card.js
+++ components/card.js
@@ -15,7 +15,7 @@ export default function Card() {
             />
           </a>
           <div class="p-6">
-            <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
+            <h5 class="text-gray-900 text-xl font-medium mb-2">Improve your front-end skills by building projects</h5>
             <p class="text-gray-700 text-base mb-4">
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
